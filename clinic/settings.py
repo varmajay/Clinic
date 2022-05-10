688af1a5ad7f32934f907524dd7bde83e32833ba
Varma Jayshankar <jayshankarvarma121@gmail.com>
2022-05-10 16:36:40 +0530

    unchanged

--- clinic/settings.py
+++ clinic/settings.py
@@ -129,7 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'gsmtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER ='jayprojecttest@gmail.com'
