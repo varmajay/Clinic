d32efe7edc8bb1e2a8e58d74a0e6638246af1d2d
Varma Jayshankar <jayshankarvarma121@gmail.com>
2022-05-18 10:07:16 +0530

    revert models

--- superadmin/models.py
+++ superadmin/models.py
@@ -76,7 +76,7 @@ class Appoinment(models.Model):
     patient_name = models.CharField(max_length=30)
     weeks = models.IntegerField()
     timeslot = models.IntegerField()
-    date =models.DateField(default=date.today())
+    date =models.DateField()
     description = models.TextField( default=None)
     status = models.IntegerField(default=0,choices=STATUS)
 
